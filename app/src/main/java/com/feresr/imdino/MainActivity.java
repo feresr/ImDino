69d508437a08298900d8cdf33c4e9d0857c76bdc
feresr <fernandoraviola@gmail.com>
2016-10-23 18:15:41 +1100

    Wire js game to android view

--- app/src/main/java/com/feresr/imdino/MainActivity.java
+++ app/src/main/java/com/feresr/imdino/MainActivity.java
@@ -1,13 +1,24 @@
 package com.feresr.imdino;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        final WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
     }
 }
